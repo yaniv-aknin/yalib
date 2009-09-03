52e7fe8846904955638a6b2788fe18350150e786
Yaniv Aknin <yaniv@aknin.name>
2009-09-03 21:18:35 +0300

    add alien.argparse

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 """
 yalib is an assorted collection of Pythonic stuff that is too trivial for a project of its own yet is not trivial enough to duplicate for each 'real' project. See docstrings, where they exist, for more info.
+
+The code in the 'alien' sub-package of yalib was written by other authors and is included verbatim or with few changes for convenience.
 """
